d442101149636f1e954a2a77b8b8a57387d44c72
nnreddybalreddy <narendra.mahn@gmail.com>
2025-05-04 23:39:16 -0400

    ALL

--- basics/helloworld.py
+++ basics/helloworld.py
@@ -1,1 +1,3 @@
 print("Hello World")
+print("Welcome to python scripting")
+print("This is my first script")
---
+++ basics/indentation.py
@@ -0,0 +1,10 @@
+print("This is about indentation")
+if 3 > 1:
+    print("3 is greater than 1")
+    print("if condition")
+else:
+    print("3 is less than 1")
+    print("Else condition")
+
+print("We are ok with indentations")
+
