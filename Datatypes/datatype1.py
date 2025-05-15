1761a493b9758ad14f23861151dd01aef66e0413
nnreddybalreddy <narendra.mahn@gmail.com>
2025-05-15 15:09:54 -0400

    ALL

---
+++ Datatypes/datatype1.py
@@ -0,0 +1,2 @@
+x=10
+print(id(x)) # 140714605675720 --> Address of x
---
+++ Datatypes/workingwithdatatypes.py
@@ -0,0 +1,98 @@
+# # # x=3 
+# # # y=4.6 
+# # # z=3+4j
+# # # number data types 
+# # # int float complex
+# # x=3;y=4.6;z=3+4j
+# # # print(x,y,z)  # 3 4.6 (3+4j) 
+# # print(x,type(x)) # 3 <class 'int'>
+# # print(y,type(y)) # 4.6 <class 'float'>
+# # print(z,type(z)) # (3+4j) <class 'complex'>
+
+# # # string data types
+
+# # my_name="narendra"  #'narendra' "narendra"  """narendra"""
+# # print(my_name) # narendra
+
+# # # Ian_name=python #
+# # # print(Ian_name) #  NameError: name 'python' is not defined
+
+
+# # my_name="x"
+# # print(my_name) # x
+
+# # my_value=True
+# # print(my_value,type(my_value)) # True <class 'bool'>
+
+# # my_new_value=False
+# # print(my_new_value,type(my_new_value)) # False <class 'bool'>
+
+
+# # # my_one_more_value=true 
+# # # print(my_one_more_value)
+# # # # 
+
+# # my_value="True"
+# # print(my_value,type(my_value)) # True <class 'str'>
+
+
+
+# ########## CONVERSION of data types
+# # int to string
+# x=56
+# print(x,type(x)) # 56 <class 'int'>
+
+# y=str(x)
+# print(y,type(y)) # 56 <class 'str'>
+# #######################################
+
+# # int to bool
+# p=0
+# print(p,type(p)) # 0 <class 'int'>
+# q=bool(p)
+# print(q,type(q)) # False <class 'bool'>
+
+
+
+
+
+# x="3.2.1.5"
+
+# print(int(x)) # value Error 
+
+# x="45"
+# print(int(X)) # 45
+
+
+###########################33
+
+x=45 
+print(bool(x)) # True 
+
+y=45.6 
+
+print(bool(y)) # True 
+
+z="Narendra"
+print(bool(z)) # True 
+
+
+print(bool(0)) # False
+print(bool("")) # False
+print(bool([])) # False
+print(type([])) # List 
+print(bool({})) # False
+print(type(({}))) # dict 
+
+
+print(bool(())) # False
+print(type(())) # tuple
+print(bool(None)) # False
+print(bool(0.0)) # False    
+print(bool(0j)) # False
+print(bool(0.0+0j)) # False    
+
+
+
+
+
