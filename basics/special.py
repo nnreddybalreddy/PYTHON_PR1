9735eab65d05138d370d6a77488cd7c91ed41556
nnreddybalreddy <narendra.mahn@gmail.com>
2025-05-15 11:32:03 -0400

    ALL

---
+++ basics/special.py
@@ -0,0 +1,7 @@
+#print("Welcome to Python scripting")
+#print("We are working with specail chars")
+
+
+print("Welcome to python scripting \nWe are working with special chars")
+
+print("Hello \bWorld")
