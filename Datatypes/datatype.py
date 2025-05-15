7192450d875c3834d8fa07eae00753ae6a15ded7
nnreddybalreddy <narendra.mahn@gmail.com>
2025-05-15 12:13:32 -0400

    ALL

---
+++ Datatypes/datatype.py
@@ -0,0 +1,38 @@
+x=4
+print(x) # 4 
+print('x') # 'x
+print("hello") # hello
+y=5.6 
+print(y) #5.6 
+
+print(type(x),type(y)) # <class 'int'> <class 'float'>
+
+x=23
+print(x) #23 
+
+del x 
+# print(x)
+
+
+#########################
+
+java_version = 1.8
+#  x$
+#   $s  not valid 
+
+#print=2 
+# print(print)
+
+my_name="narendra"
+print(my_name) # narendra letters , numbers,_
+
+
+# my_ name = "narendra"  #dont give spaces 
+# 4x56=22.6  -- Do not start with number 
+
+x=10
+print(x) # 10 case sensitive 
+X=11 
+print(X) # 11
+
+_x=10 # can start with _ not with number
